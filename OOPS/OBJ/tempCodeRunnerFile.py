19ac5bcb1362ba0425ab5f44ceb35c1d10478263
Kartik Pant <kartikpant.kp69@gmail.com>
2025-08-30 10:53:27 +0530

    polymorphism

--- OOPS/OBJ/tempCodeRunnerFile.py
+++ OOPS/OBJ/tempCodeRunnerFile.py
@@ -1,9 +1,24 @@
-class Car():
-    def __init__(self,brand,color):
-        self.brand=brand
-        self.color=color
+class action():
+    def fight(self):
+        print(f"We are good to go ! ")
 
-car1=Car('mercedes','black')
-car2=Car('lambo','green')
-print(car1.brand,car1.color)
-print(car2.brand,car2.color)
+class boeing(action):
+    def flight(self):
+        print("Runway Free !")
+        print(f"The Boeing 777 is set to fly lets go ! ")
+        
+        print(" GEAR UP !")
+
+class airbus(action):
+    def flight(self):
+        print("Runway Free !")
+        print(f"The Airbus A380 is set to fly lets go ! ")
+        
+        print(" GEAR UP !")
+        
+
+Runway1=boeing()
+Runway2=airbus()
+
+Runway1.flight()
+Runway2.flight()
