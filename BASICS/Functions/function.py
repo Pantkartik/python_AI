0c9dfc945d72dfdbe6d0dff61a76cdb0faed3eb3
Kartik Pant <kartikpant.kp69@gmail.com>
2025-09-08 14:02:50 +0530

    Update function.py

--- BASICS/Functions/function.py
+++ BASICS/Functions/function.py
@@ -1,54 +1,61 @@
-# declaring the function 
-def hello(name):
-  print("Hello,",name)
+# # declaring the function 
+# def hello(name):
+#   print("Hello,",name)
 
-name=input("Enter your name : ")
-hello(name)
-
-
-# to calculate the sum of two numbers usinf functions
-
-def sum_num(a,b):
-  return a+b
-
-a=int(input("NUMBER : "))
-b=int(input("NUMBER : "))
-print(sum_num(a,b))
+# name=input("Enter your name : ")
+# hello(name)
 
 
-#  to find the valid age for DL
+# # to calculate the sum of two numbers usinf functions
 
-def valid_age(age):
-  if age>=18 and age<=60  :
-    print("Valid")
+# def sum_num(a,b):
+#   return a+b
+
+# a=int(input("NUMBER : "))
+# b=int(input("NUMBER : "))
+# print(sum_num(a,b))
+
+
+# #  to find the valid age for DL
+
+# def valid_age(age):
+#   if age>=18 and age<=60  :
+#     print("Valid")
  
-  else :
-    print("Not Valid")
-age=int(input("Enter the age : "))
-valid_age(age)
+#   else :
+#     print("Not Valid")
+# age=int(input("Enter the age : "))
+# valid_age(age)
 
 
-#  using main function to define functions anywhere in the program
+# #  using main function to define functions anywhere in the program
 
 
 
-def main():
-  name=input("Enter the name : ")
-  hello(name)
+# def main():
+#   name=input("Enter the name : ")
+#   hello(name)
 
-def hello(to):
-  print("Hello, ",to)
+# def hello(to):
+#   print("Hello, ",to)
 
-main()
+# main()
 
 
-# return function 
+# # return function 
 
-def main():
-  x=int(input("Enter the number : "))
-  print("Square of x is ",square(x))
+# def main():
+#   x=int(input("Enter the number : "))
+#   print("Square of x is ",square(x))
 
-def square(n):
-  return n*n
+# def square(n):
+#   return n*n
 
-main()
+# main()
+
+
+def function(n,m):
+  print("Hello world")
+  print(n+m)
+  
+function(3,2)
